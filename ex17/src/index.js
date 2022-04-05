f74c48755d3a9f2f1a06a7472fed35220b0cb774
melikarameli <melikahatunic2001@gmail.com>
2022-04-05 18:10:13 +0200

    Added ex17

---
+++ ex17/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function inverseWhile() {
+    var fiveNumbers = 0;
+    while(fiveNumbers < 5) {
+        console.log(fiveNumbers);
+        fiveNumbers++;
+    }
+    return fiveNumbers;
+}
+// Only change code above this line
+console.log(inverseWhile());
+module.exports = inverseWhile;
