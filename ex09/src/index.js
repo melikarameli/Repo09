7136cb1cae9194692db4725bae6c2fc4af95dcaf
melikarameli <melikahatunic2001@gmail.com>
2022-04-05 20:27:40 +0200

    Added ex09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,9 +1,9 @@
 // Only change code below this line
 function logicalOrOperator(num) {
-    if (num >= 20 || num <= 30) {
-        return "In";
+    if (num < 20 || num > 30) {
+        return "Out";
     }
-    return "Out";
+    return "In";
 }
 console.log(logicalOrOperator(0));
 console.log(logicalOrOperator(9));
